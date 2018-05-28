9d27df66ee845bc840005f938776c8f14da05e96
huazai <hzof@qq.com>
2018-05-28 17:43:41 +0800

    测试

--- src/test/java/com/cloudht/testDemo/TestDemo.java
+++ src/test/java/com/cloudht/testDemo/TestDemo.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class TestDemo {
-public String ss="111";
+public String ss="1啊啊11";
 
 }
