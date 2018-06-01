aa55b9f576f76e611d28eb12071d8ee4320336e8
yuxueling <yuxueling08@163.com>
2018-06-01 17:07:51 +0800

    更换logo

--- src/main/java/com/cloudht/system/controller/LoginController.java
+++ src/main/java/com/cloudht/system/controller/LoginController.java
@@ -50,10 +50,10 @@ public class LoginController extends BaseController {
 			if(fileService.isExist(fileDO.getUrl())){
 				model.addAttribute("picUrl",fileDO.getUrl());
 			}else {
-				model.addAttribute("picUrl","/img/photo_s3.jpg");
+				model.addAttribute("picUrl","/img/photo_s2.png");
 			}
 		}else {
-			model.addAttribute("picUrl","/img/photo_s3.jpg");
+			model.addAttribute("picUrl","/img/photo_s2.png");
 		}
 		model.addAttribute("username", getUser().getUsername());
 		return "index_v1";
